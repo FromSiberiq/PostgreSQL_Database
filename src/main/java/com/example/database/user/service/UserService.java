7016030efe84e904ffa54f67ff0a75133a4f9aa8
Okami <mr.naydyuk2@gmail.com>
2025-02-20 16:54:44 +0800

    Реализация генерации пользователей

---
+++ src/main/java/com/example/database/user/service/UserService.java
@@ -0,0 +1,37 @@
+package com.example.database.user.service;
+
+import com.example.database.user.entity.UserEntity;
+import com.example.database.user.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class UserService {
+    private final UserRepository userRepository;
+    private final NamesGenerator namesGenerator = new NamesGenerator();
+
+    public boolean init() {
+        userRepository.deleteAll();
+
+        int repeat = 10, dataPerLoop = 10000;
+        for (int i = 0; i < repeat; i++) {
+            List<UserEntity> all = new ArrayList<>();
+            for (int dataIndex = 0; dataIndex < dataPerLoop; dataIndex++) {
+                NamesGenerator.Name name = namesGenerator.generate();
+                UserEntity user = UserEntity.builder()
+                        .lastName(name.getLastName())
+                        .firstName((name.getFirstName()))
+                        .build();
+                all.add(user);
+            }
+            userRepository.saveAll(all);
+        }
+        return true;
+    }
+
+
+}
